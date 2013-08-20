e69c3250c746c25036de33268befc7f973e47542
Andrew Vos <andrew.vos@gmail.com>
2013-08-20 13:06:52 +0100

    Should probably return some text

--- colour.go
+++ colour.go
@@ -4,15 +4,15 @@ import (
   "strconv"
 )
 
-func Black(text string)   { colour(text, 0) }
-func Red(text string)     { colour(text, 1) }
-func Green(text string)   { colour(text, 2) }
-func Yellow(text string)  { colour(text, 3) }
-func Blue(text string)    { colour(text, 4) }
-func Magenta(text string) { colour(text, 5) }
-func Cyan(text string)    { colour(text, 6) }
-func White(text string)   { colour(text, 7) }
-func Default(text string) { colour(text, 9) }
+func Black(text string)   string { return colour(text, 0) }
+func Red(text string)     string { return colour(text, 1) }
+func Green(text string)   string { return colour(text, 2) }
+func Yellow(text string)  string { return colour(text, 3) }
+func Blue(text string)    string { return colour(text, 4) }
+func Magenta(text string) string { return colour(text, 5) }
+func Cyan(text string)    string { return colour(text, 6) }
+func White(text string)   string { return colour(text, 7) }
+func Default(text string) string { return colour(text, 9) }
 
 func colour(text string, colour int) string {
   return "\x1b[3"+strconv.Itoa(colour)+";1m" + text + "\x1b[0m"
