25a09b77a220c621814460a44f318cd0686c8ee5
Andrew Vos <andrew.vos@gmail.com>
2014-09-23 15:20:56 +0100

    Allow disabling of colour output

--- colour.go
+++ colour.go
@@ -1,19 +1,24 @@
 package colour
 
 import (
-  "strconv"
+	"strconv"
 )
 
-func Black(text string)   string { return colour(text, 0) }
-func Red(text string)     string { return colour(text, 1) }
-func Green(text string)   string { return colour(text, 2) }
-func Yellow(text string)  string { return colour(text, 3) }
-func Blue(text string)    string { return colour(text, 4) }
+var Enabled = true
+
+func Black(text string) string   { return colour(text, 0) }
+func Red(text string) string     { return colour(text, 1) }
+func Green(text string) string   { return colour(text, 2) }
+func Yellow(text string) string  { return colour(text, 3) }
+func Blue(text string) string    { return colour(text, 4) }
 func Magenta(text string) string { return colour(text, 5) }
-func Cyan(text string)    string { return colour(text, 6) }
-func White(text string)   string { return colour(text, 7) }
+func Cyan(text string) string    { return colour(text, 6) }
+func White(text string) string   { return colour(text, 7) }
 func Default(text string) string { return colour(text, 9) }
 
 func colour(text string, colour int) string {
-  return "\x1b[3"+strconv.Itoa(colour)+";1m" + text + "\x1b[0m"
+	if Enabled {
+		return "\x1b[3" + strconv.Itoa(colour) + ";1m" + text + "\x1b[0m"
+	}
+	return text
 }
